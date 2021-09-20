75c873474d900c37f3d971747978b92a79922e96
leftygray <leftygray@gmail.com>
2021-09-20 22:27:19 +1000

    Put file movement scripts into functions in a single script

---
+++ bin/mvfiles.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# This script contains a set of functions for moving files around and "pulling"
+# files from other main directories   
+#
+
+glf() {
+	# A little function to retrieve the last modified file in $wi/Documents/Inbox. 
+
+	find $wi -type f -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1 | xargs -0 -d "\n" -I {} mv {} ./
+
+	echo "Latest file from Inbox retrieved..."
+}
+
+
+
+gfib() {
+	# A little function to retrieve a file from $wh/Documents/Inbox. 
+
+	mv $wi/"$1" ./
+
+	echo "File $1 from Inbox retrieved..."
+}
+
+
+getfile() {
+	# A little function to get a file into the current directory
+
+	mv "$1" ./
+	echo "Moved $1 into pwd..."
+}
